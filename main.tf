160c069f2f7e9358794c595f6c88496d45895e27
Mikhail Naletov <36846182+okgolove@users.noreply.github.com>
2019-12-30 17:34:39 +0200

    First version of the module (#1)
    * First version of the module
    
    * Change delimeter for base label

---
+++ main.tf
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2019 Risk Focus, Inc. - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the Apache License Version 2.0.
+ * http://www.apache.org/licenses
+ */
+
+provider "aws" {
+  alias   = "master"
+  region  = var.aws_region
+  version = "~> 2.0"
+}
+
+provider "aws" {
+  alias  = "subaccount"
+  region = var.aws_region
+
+  assume_role {
+    role_arn = "arn:aws:iam::${aws_organizations_account.account.id}:role/${aws_organizations_account.account.role_name}"
+  }
+}
+
+module "base_label" {
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  namespace   = var.org_rev_fqdn
+  environment = var.project_prefix
+}
